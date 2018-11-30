199c1027056b06d4b3b0b05d06e3c4c8ba8e7b82
kakawkaization <kakawkaization@gmail.com>
2018-11-30 19:07:23 +0300

    225

---
+++ Override223.java
@@ -0,0 +1,30 @@
+class A {
+    int i, j;
+    A(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    void show() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
+
+class B extends A {
+    int k;
+    B(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    void show() {
+        System.out.println("k: " + k);
+    }
+}
+
+class Override223 {
+    public static void main(String args[]) {
+        B subOb = new B(1, 2, 3);
+        subOb.show();
+    }
+}

e91a1988b99def80330aeb0b2b7690c852cbf00f
kakawkaization <kakawkaization@gmail.com>
2018-12-08 15:28:24 +0300

    244

---
+++ TestBalance244.java
@@ -0,0 +1,9 @@
+import MyPack243.*;
+
+class TestBalance244 {
+    public static void main(String args[]) {
+        Balance test = new Balance("J. J. Jaspers", 99.88);
+
+        test.show();
+    }
+}

d506da7fc735d874dde16cfdee0f5fa535054d6c
kakawkaization <kakawkaization@gmail.com>
2019-01-04 12:22:09 +0300

    266

---
+++ Exc0_265.java
@@ -0,0 +1,6 @@
+class Exc0_265 {
+    public static void main(String args[]) {
+        int d = 0;
+        int a = 42 / d;
+    }
+}
---
+++ Exc1_266.java
@@ -0,0 +1,9 @@
+class Exc1_266 {
+    static void subroutine() {
+        int d = 0;
+        int a = 42 / d;
+    }
+    public static void main(String args[]) {
+        Exc1_266.subroutine();
+    }
+}
